4e524bcf53585d3db03ef2087f79e1e9422ad529
yingle.an <yingle.an@envision-energy.com>
2021-04-14 19:33:55 +0800

    ay

--- it-gateway-8081/src/main/java/com/ay/config/ZuulAdvance.java
+++ it-gateway-8081/src/main/java/com/ay/config/ZuulAdvance.java
@@ -5,20 +5,28 @@ import com.netflix.loadbalancer.IRule;
 import com.netflix.loadbalancer.Server;
 import com.netflix.niws.loadbalancer.DiscoveryEnabledServer;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang.StringUtils;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Slf4j
 public class ZuulAdvance implements IRule {
 
+    private AtomicInteger nextServerCyclicCounter;
     private ILoadBalancer loadBalancer;
 
-    @Override
-    public void setLoadBalancer(ILoadBalancer iLoadBalancer) {
-        this.loadBalancer=iLoadBalancer;
+    public ZuulAdvance() {
+        nextServerCyclicCounter = new AtomicInteger(0);
+    }
+
+    private int incrementAndGetModulo(int modulo) {
+        for (; ; ) {
+            int current = nextServerCyclicCounter.get();
+            int next = (current + 1) % modulo;
+            if (nextServerCyclicCounter.compareAndSet(current, next)) {
+                return next;
+            }
+        }
     }
 
     @Override
@@ -27,23 +35,48 @@ public class ZuulAdvance implements IRule {
     }
 
     @Override
-    public Server choose(Object key) {
-        String region = "bj";
+    public void setLoadBalancer(ILoadBalancer iLoadBalancer) {
+        this.loadBalancer = iLoadBalancer;
+    }
 
-        if (loadBalancer == null) {
+    @Override
+    public Server choose(Object key) {
+        ILoadBalancer lb = this.getLoadBalancer();
+        List<Server> servers = this.loadBalancer.getAllServers();
+        log.info("server size now have {}", servers.size());
+        for (Server server : servers) {
+            DiscoveryEnabledServer se = (DiscoveryEnabledServer) server;
+            log.info(se.toString());
+        }
+        if (lb == null) {
             log.warn("no load balancer");
             return null;
         }
-
-        List<Server> servers= loadBalancer.getAllServers();
-
-        List<DiscoveryEnabledServer> instance = new ArrayList<>();
-
-        for (Server server : servers) {
-            DiscoveryEnabledServer se = (DiscoveryEnabledServer) server;
-            instance.add(se);
+        Server server;
+        int count = 0;
+        while (count++ < 10) {
+            List<Server> reachableServers = lb.getReachableServers();
+            List<Server> allServers = lb.getAllServers();
+            int upCount = reachableServers.size();
+            int serverCount = allServers.size();
+            if ((upCount == 0) || (serverCount == 0)) {
+                log.warn("No up servers available from load balancer: " + lb);
+                return null;
+            }
+            int nextServerIndex = incrementAndGetModulo(serverCount);
+            server = allServers.get(nextServerIndex);
+            if (server == null) {
+                Thread.yield();
+                continue;
+            }
+            if (server.isAlive() && (server.isReadyToServe())) {
+                return (server);
+            }
         }
-
-        return servers.get(0);
+        if (count >= 10) {
+            log.warn("No available alive servers after 10 tries from load balancer: "
+                    + lb);
+        }
+        return null;
     }
 }
